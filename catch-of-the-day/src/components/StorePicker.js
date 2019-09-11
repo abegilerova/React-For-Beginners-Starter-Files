2f31dc21be65242c93cc268c1485031e522233a2
abegilerova <abegilerova@gmail.com>
2019-09-11 18:31:16 -0400

    adding ref property to Store Picker

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { getFunName } from "../helpers";
+
+class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    //instead of bining in constructor we transformed function into property
+    goToStore = (event) => {
+        //stop the form from submitting
+        event.preventDefault();
+        //2. get the text from that input
+        console.log(this);
+        //3. change the page to /store/whatever-they-entered
+
+
+    }
+    render() {
+
+        return (
+            <>
+                <form className="store-selector" onSubmit={this.goToStore}>
+                    <h2>Please enter A Store</h2>
+                    <input
+                        type="text"
+                        ref={this.myInput}
+                        required placeholder="Store Name"
+                        defaultValue={getFunName()} />
+                    <button type="submit">Visit Store -></button>
+                </form>
+            </>
+        )
+    }
+}
+
+export default StorePicker;
+
